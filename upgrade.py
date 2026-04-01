e8bf747e0d0a0ede883f08c13c37f6fdac982438
lulu094 <lucci.venanciomostaceli@ucas-edu.net>
2026-04-01 09:51:50 -0600

    started writing pseudocode

---
+++ upgrade.py
@@ -0,0 +1,12 @@
+# LV 1st Upgrading RPG Manager
+# import pandas, matplotlib, faker
+
+# Learn how to use Pandas to show bar graphs
+# graphs have to show the calculated stistical metrics (mean, median, max, min) for character attributes across roster
+# save/ export generated charts
+
+# add more options to the main menu and sub options to show the user the charts and new things it can do
+
+# Faker
+# make realistic names and backstories
+# implement rendom quesr generation
